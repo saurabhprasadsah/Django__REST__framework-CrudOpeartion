16907e153a3aa28073742940c4247454357205b0
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-08-10 19:12:22 +0530

    intial commit

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from models import Employee
 # Register your models here.
+
+@admin.register()
+class Employee(admin.ModelAdmin):
+    list_display=['id','name','email','phone','dsg','city', 'state', ]
--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    id = models.AutoField(primary_key=True)
+    name= models.CharField(max_length=30)
+    email = models.CharField(max_length=15)
+    phone =models.CharField(max_length=15)
+    dsg = models.CharField(max_length=50)
+    salary = models.IntegerField()
+    city = models.CharField(max_length=30, default="", null= True, blank= True)
+    state = models.CharField(max_length=30, default="", null= True, blank= True)
+
+
+
+
